e1328479826edf86bbc38d4a72246977dba386b7
ostatni5 <26521377+ostatni5@users.noreply.github.com>
2021-08-23 12:43:14 +0200

    emmbded editor into react

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import ThreeEditor from './editor/ThreeEditor';
+import ThreeEditor from './ThreeEditor/ThreeEditor';
+
 
 
 function App() {
--- src/ThreeEditor/js/Sidebar.Geometry.js
+++ src/ThreeEditor/js/Sidebar.Geometry.js
@@ -1,4 +1,4 @@
-import * as THREE from '../../build/three.module.js';
+import * as THREE from 'three'
 
 import { UIPanel, UIRow, UIText, UIInput, UIButton, UISpan } from './libs/ui.js';
 
@@ -7,7 +7,7 @@ import { SetGeometryValueCommand } from './commands/SetGeometryValueCommand.js';
 import { SidebarGeometryBufferGeometry } from './Sidebar.Geometry.BufferGeometry.js';
 import { SidebarGeometryModifiers } from './Sidebar.Geometry.Modifiers.js';
 
-import { VertexNormalsHelper } from '../../examples/jsm/helpers/VertexNormalsHelper.js';
+import { VertexNormalsHelper } from './libs/helpers/VertexNormalsHelper.js';
 
 function SidebarGeometry( editor ) {
 
