5492ce6649ecb5b9ade6e7242d056fa023a115c2
derstilon <quban123@gmail.com>
2021-08-26 08:31:24 +0200

    Add ZoneManager

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import ThreeEditor from './ThreeEditor/ThreeEditor';
+import JsRoot from './JsRoot/JsRoot';
+import ZoneManagerPanel from './components/ZoneManagerPanel/ZoneManagerPanel';
 
 
 
 function App() {
-  return (
-    <ThreeEditor></ThreeEditor>
-  );
+  return (<>
+    <JsRoot></JsRoot>
+    <ZoneManagerPanel></ZoneManagerPanel>
+  </>);
 }
 
 export default App;
---
+++ src/components/ZoneManagerPanel/BooleanAlgebraRow.tsx
@@ -0,0 +1,9 @@
+type BooleanAlgebraRowProps = {
+    id: number
+}
+
+function BooleanAlgebraRow(props:BooleanAlgebraRowProps) {
+    return <div>SAME SAME {props.id}</div>
+}
+
+export default BooleanAlgebraRow;
