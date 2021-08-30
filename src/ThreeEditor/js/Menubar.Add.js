96b8483df6b582da9c8f134ad4d189abaaa34e48
ostatni5 <26521377+ostatni5@users.noreply.github.com>
2021-08-30 16:32:07 +0200

    change default material

--- src/ThreeEditor/js/Menubar.Add.js
+++ src/ThreeEditor/js/Menubar.Add.js
@@ -4,76 +4,79 @@ import { UIPanel, UIRow, UIHorizontalRule } from './libs/ui.js';
 
 import { AddObjectCommand } from './commands/AddObjectCommand.js';
 
-function MenubarAdd( editor ) {
+function MenubarAdd(editor) {
 
 	var strings = editor.strings;
 
 	var container = new UIPanel();
-	container.setClass( 'menu' );
+	container.setClass('menu');
 
 	var title = new UIPanel();
-	title.setClass( 'title' );
-	title.setTextContent( strings.getKey( 'menubar/add' ) );
-	container.add( title );
+	title.setClass('title');
+	title.setTextContent(strings.getKey('menubar/add'));
+	container.add(title);
 
 	var options = new UIPanel();
-	options.setClass( 'options' );
-	container.add( options );
+	options.setClass('options');
+	container.add(options);
 
 	// Group
 
 	var option = new UIRow();
-	option.setClass( 'option' );
-	option.setTextContent( strings.getKey( 'menubar/add/group' ) );
-	option.onClick( function () {
+	option.setClass('option');
+	option.setTextContent(strings.getKey('menubar/add/group'));
+	option.onClick(function () {
 
 		var mesh = new THREE.Group();
 		mesh.name = 'Zone';
 
-		editor.execute( new AddObjectCommand( editor, mesh ) );
+		editor.execute(new AddObjectCommand(editor, mesh));
 
-	} );
-	options.add( option );
+	});
+	options.add(option);
 
 	//
 
-	options.add( new UIHorizontalRule() );
+	options.add(new UIHorizontalRule());
+
+	const material = new THREE.MeshStandardMaterial({ wireframe: true, opacity: .2, transparent: true });
+
 
 	// Box
 
 	var option = new UIRow();
-	option.setClass( 'option' );
-	option.setTextContent( strings.getKey( 'menubar/add/box' ) );
-	option.onClick( function () {
+	option.setClass('option');
+	option.setTextContent(strings.getKey('menubar/add/box'));
+	option.onClick(function () {
 
-		var geometry = new THREE.BoxGeometry( 1, 1, 1, 1, 1, 1 );
-		var mesh = new THREE.Mesh( geometry, new THREE.MeshStandardMaterial() );
+		var geometry = new THREE.BoxGeometry(1, 1, 1, 1, 1, 1);
+		var mesh = new THREE.Mesh(geometry, material);
 		mesh.name = 'Box';
 
-		editor.execute( new AddObjectCommand( editor, mesh ) );
+		editor.execute(new AddObjectCommand(editor, mesh));
 
-	} );
-	options.add( option );
+	});
+	options.add(option);
 
-	
+
 	// Sphere
 
 	var option = new UIRow();
-	option.setClass( 'option' );
-	option.setTextContent( strings.getKey( 'menubar/add/sphere' ) );
-	option.onClick( function () {
+	option.setClass('option');
+	option.setTextContent(strings.getKey('menubar/add/sphere'));
+	option.onClick(function () {
 
-		var geometry = new THREE.SphereGeometry( 1, 32, 16, 0, Math.PI * 2, 0, Math.PI );
-		var mesh = new THREE.Mesh( geometry, new THREE.MeshStandardMaterial() );
+		var geometry = new THREE.SphereGeometry(1, 16, 8, 0, Math.PI * 2, 0, Math.PI);
+		var mesh = new THREE.Mesh(geometry, material);
 		mesh.name = 'Sphere';
 
-		editor.execute( new AddObjectCommand( editor, mesh ) );
+		editor.execute(new AddObjectCommand(editor, mesh));
 
-	} );
-	options.add( option );
-	
+	});
+	options.add(option);
 
-	options.add( new UIHorizontalRule() );
+
+	options.add(new UIHorizontalRule());
 
 
 	return container;
