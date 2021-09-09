4be666162fa7830872285bac0f8226bffd048339
derstilon <quban123@gmail.com>
2021-09-09 11:04:38 +0200

    Fix MaterialUI Strict mode warning

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css';
 import ThreeEditor from './ThreeEditor/ThreeEditor';
-
-
+import JsRoot from './JsRoot/JsRoot';
+import ZoneManagerPanel from './components/ZoneManagerPanel/ZoneManagerPanel';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { unstable_createMuiStrictModeTheme as createMuiTheme } from '@material-ui/core';
 
 function App() {
-  return (<>
+  const theme = createMuiTheme();
+  return (<ThemeProvider theme={theme}>
+    {/* <JsRoot></JsRoot> */}
+
     <ThreeEditor></ThreeEditor>
-  </>);
+  </ThemeProvider>);
 }
 
 export default App;
