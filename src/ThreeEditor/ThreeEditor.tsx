9d3024a7d267cda5f4183ba74eae3f56c3d52de3
ostatni5 <26521377+ostatni5@users.noreply.github.com>
2021-09-10 11:35:51 +0200

    add multiple views

--- src/ThreeEditor/ThreeEditor.tsx
+++ src/ThreeEditor/ThreeEditor.tsx
@@ -20,45 +20,6 @@ function ThreeEditor() {
       const { editor, viewport, toolbar, sidebar, menubar, resizer } = initEditor(containerEl.current);
       setEditor(editor);
 
-      // Make 2 box meshes..
-
-      let meshA = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1));
-      let meshB = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1));
-
-      //offset one of the boxes by half its width..
-
-      meshB.position.add(new THREE.Vector3(0.5, 0.5, 0.5));
-
-      //Make sure the .matrix of each mesh is current
-
-      meshA.updateMatrix();
-      meshB.updateMatrix();
-
-      //Create a bsp tree from each of the meshes
-
-      let bspA = CSG.fromMesh(meshA);
-      let bspB = CSG.fromMesh(meshB);
-
-      // Subtract one bsp from the other via .subtract... other supported modes are .union and .intersect
-
-      let bspResult = bspA.subtract(bspB);
-
-      //Get the resulting mesh from the result bsp, and assign meshA.material to the resulting mesh
-
-      let meshResult = CSG.toMesh(bspResult, meshA.matrix, meshA.material);
-
-      meshResult.name = 'meshResult';
-      meshResult.material = new THREE.MeshNormalMaterial();
-      // editor.sceneHelpers.add(meshResult);
-
-
-      // let manager = new CSGManager(editor);
-      // let zone = manager.createZone();
-
-      // zone.addUnion();
-      // zone.addOperation(0, { object: meshA, mode: 'union' });
-      // zone.addOperation(0, { object: meshB, mode: 'left-subtraction' });
-
     }
     return () => {
 
@@ -71,7 +32,7 @@ function ThreeEditor() {
       {editor &&
         <>
           <SampleComponent signal={editor.signals.objectSelected} ></SampleComponent>
-          <ZoneManagerPanel editor={editor}></ZoneManagerPanel>
+          {/* <ZoneManagerPanel editor={editor}></ZoneManagerPanel> */}
         </>
       }
     </div>
