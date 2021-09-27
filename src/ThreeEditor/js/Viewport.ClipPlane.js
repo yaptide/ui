f10804194b0a333ffc17af9fe05ae408d516096a
Leszek Grzanka <leszek.grzanka@gmail.com>
2021-09-27 18:32:54 +0200

    documenting code

--- src/ThreeEditor/js/Viewport.ClipPlane.js
+++ src/ThreeEditor/js/Viewport.ClipPlane.js
@@ -1,9 +1,23 @@
 import * as THREE from 'three';
 import { GUI } from 'three/examples//jsm/libs/dat.gui.module.js';
 
+/*
+This function is responsible for preparing scene with clipping plane and stencil materials
+It creates and manages controls for shifting clipping planes in X,Y,Z axis.
+Stencil materials are used as a trick in order to avoid display of transparent inner walls of clipped objects (zones/bodies).
+We aim at assigning stencil materials to hold different colors than the walls of the objects and represent the internal material which is filling the zone.
+This is especially useful for such clipped views where at the same time internal material and wall of the object is visible at same time
+(for example torus clipped by a plane containing its axis of symmetry, stencil material fills two full circles, while wall material is drawn on the visible outer wall).
+ */
 export function ViewportClippedView(editor, viewport, planeHelpers, initialObjects, signalGeometryChanged, { clipPlane, planeHelperColor, planePosLabel }) {
 
+    // default order is zero, we assign higher order to stencil plane to display it in front of other objects
     const STENCIL_RENDER_ORDER = 1;
+
+    // several objects we are using (stencil plan) are limited to a square on a plane with given size (side length)
+    // it limits range of applicability of clipping plane
+    // it also limits the size (horizontally and vertically) of stencil plane
+    // here side length is set to 10 cm
     const CLIPPING_SIZE = 10;
 
     this.scene = new THREE.Scene();
@@ -19,13 +33,18 @@ export function ViewportClippedView(editor, viewport, planeHelpers, initialObjec
 
         get [planePosProperty]() {
 
+            // distance from a plane to the origin of coordinate system
+            // as our planes are parallel to OXY, OYZ or OXZ, this reduces to the distance along normal vector
             return clipPlane.constant;
 
         },
         set [planePosProperty](v) {
 
-            crossSectionPlane.translateZ(v - clipPlane.constant); // translate Cross Section Plane with clip plane
+            // take the previous location of clipping plane and shift stencil plane to the desired location of clipping plane
+            // we translate in the plane in local coordinate system
+            stencilPlane.translateZ(v - clipPlane.constant);
 
+            // align clipping plane with stencil plane
             clipPlane.constant = v;
 
             editor.signals.viewportConfigChanged.dispatch({ name: viewport.name, globalPlaneConstant: v });
@@ -48,7 +67,9 @@ export function ViewportClippedView(editor, viewport, planeHelpers, initialObjec
 
     };
 
-    this.gui.add(uiProps, planePosProperty, -CLIPPING_SIZE, CLIPPING_SIZE, 0.1);
+    // adjust range of movement of clipping plane to -size...+size with given step
+    const clipPlaneStep = 0.1;
+    this.gui.add(uiProps, planePosProperty, -CLIPPING_SIZE, CLIPPING_SIZE, clipPlaneStep);
     this.gui.add(uiProps, 'Helper Visible', true);
     this.gui.domElement.style.position = 'absolute';
     this.gui.domElement.style.top = '35px';
@@ -56,7 +77,7 @@ export function ViewportClippedView(editor, viewport, planeHelpers, initialObjec
 
 
     // setup plane to display where geometry is clipped
-
+    // in fact this is a square being a subset of plane
     const planeGeom = new THREE.PlaneGeometry(CLIPPING_SIZE, CLIPPING_SIZE);
 
     const clippedObjects = new THREE.Group();
@@ -79,30 +100,36 @@ export function ViewportClippedView(editor, viewport, planeHelpers, initialObjec
 
         });
 
-    const crossSectionPlane = new THREE.Mesh(planeGeom, planeMat); // Cross Section Plane - fill stencil
-    crossSectionPlane.onAfterRender = function (renderer) {
+    const stencilPlane = new THREE.Mesh(planeGeom, planeMat); // Cross Section Plane - fill stencil
+    stencilPlane.onAfterRender = function (renderer) {
 
         renderer.clearStencil();
 
     };
-    crossSectionPlane.renderOrder = STENCIL_RENDER_ORDER + .1; // must be rendered after clipped objects
+    // increase render order to have stencil plane being rendered after other objects
+    // we increase the render object by 0.1 following the original code from threejs repo
+    stencilPlane.renderOrder = STENCIL_RENDER_ORDER + .1;
 
-    crossSectionGroup.add(crossSectionPlane);
+    crossSectionGroup.add(stencilPlane);
 
-    // align mesh plane to clipping plane 
-    clipPlane.coplanarPoint(crossSectionPlane.position);
-    crossSectionPlane.lookAt(
-        crossSectionPlane.position.x - clipPlane.normal.x,
-        crossSectionPlane.position.y - clipPlane.normal.y,
-        crossSectionPlane.position.z - clipPlane.normal.z,
+    // align stencil plane to clipping plane
+    clipPlane.coplanarPoint(stencilPlane.position);
+    stencilPlane.lookAt(
+        stencilPlane.position.x - clipPlane.normal.x,
+        stencilPlane.position.y - clipPlane.normal.y,
+        stencilPlane.position.z - clipPlane.normal.z,
     );
 
-    crossSectionPlane.translateZ(clipPlane.constant); // copy distance[from (0,0,0)] from clip plane to Cross Section Plane
+    // put stencil plane at the same distance from the origin of coord. system as clipping plane
+    // in fact there will be completely aligned then
+    stencilPlane.translateZ(clipPlane.constant);
 
-    crossSectionPlane.translateZ(-.01); // offset Cross Section Plane to not be clipped by renderer
+    // move back stencil plane by epsilon distance (here 1nm)
+    // TODO why back and not to the front ?
+    // TODO try to decrease this value in such way that creating geometry of small objects (i.e. um size is still possible)
+    stencilPlane.translateZ(-1e-2);
 
     // Initialize view with objects
-
     initialObjects.forEach((object3D, index) => {
         const stencilGroup = createPlaneStencilGroup(object3D.geometry, clipPlane, STENCIL_RENDER_ORDER);
         stencilGroup.name = object3D.uuid;
@@ -110,8 +137,7 @@ export function ViewportClippedView(editor, viewport, planeHelpers, initialObjec
 
     });
 
-    // Signal
-
+    // update stencil materials when geometry of bodies and zones is updated
     signalGeometryChanged.add(function (object3D) {
         clippedObjects.remove(clippedObjects.getObjectByName(object3D.uuid));
 
@@ -122,7 +148,7 @@ export function ViewportClippedView(editor, viewport, planeHelpers, initialObjec
     });
 
     // https://github.com/mrdoob/three.js/blob/r132/examples/webgl_clipping_stencil.html
-    // Creates mask with stencil where Cross Section Plane should be visible
+    // Creates mask with stencil where stencil plane should be visible
     function createPlaneStencilGroup(geometry, plane, renderOrder) {
 
         const group = new THREE.Group();
