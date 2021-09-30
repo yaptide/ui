3ac5c02c41955d95088fef1962ec5fd1bef1f24e
derstilon <quban123@gmail.com>
2021-09-30 16:58:34 +0200

    Format ZoneMaterial document

--- src/ThreeEditor/js/Sidebar.Material.ZoneMaterial.js
+++ src/ThreeEditor/js/Sidebar.Material.ZoneMaterial.js
@@ -6,23 +6,23 @@ import { SidebarMaterialConstantProperty } from './Sidebar.Material.ConstantProp
 import { SidebarMaterialNumberProperty } from './Sidebar.Material.NumberProperty.js';
 import { SetMaterialCommand } from './commands/SetMaterialCommand.js';
 import { materialOptions } from './Editor.Materials.js';
-export default class ZoneMaterial extends UIPanel{  
-    editor;
-    constructor(editor) {
-        super();
-        this.editor = editor;
-        makeSidebarMaterialOptions(this, editor);
-    }
+export default class ZoneMaterial extends UIPanel {
+	editor;
+	constructor(editor) {
+		super();
+		this.editor = editor;
+		makeSidebarMaterialOptions(this, editor);
+	}
 }
 
 // Code copied (and adjusted) from SidebarMaterial.js to get focus function that was missing on OrbitControls.
 // https://github.com/mrdoob/three.js/blob/r132/editor/js/Sidebar.Material.js
 function makeSidebarMaterialOptions(container, editor) {
-	container.setBorderTop( '0' );
-	container.setDisplay( 'none' );
-	container.setPaddingTop( '20px' );
+	container.setBorderTop('0');
+	container.setDisplay('none');
+	container.setPaddingTop('20px');
 
-    const signals = editor.signals;
+	const signals = editor.signals;
 	const strings = editor.strings;
 	const materials = editor.simulationMaterials;
 
@@ -31,39 +31,39 @@ function makeSidebarMaterialOptions(container, editor) {
 	let currentMaterialSlot = 0;
 
 
-    // Current material slot
+	// Current material slot
 
 	const materialSlotRow = new UIRow();
 
-	materialSlotRow.add( new UIText( strings.getKey( 'sidebar/material/slot' ) ).setWidth( '90px' ) );
+	materialSlotRow.add(new UIText(strings.getKey('sidebar/material/slot')).setWidth('90px'));
 
-	const materialSlotSelect = new UISelect().setWidth( '170px' ).setFontSize( '12px' ).onChange( update );
-	materialSlotSelect.setOptions( { 0: '' } ).setValue( 0 );
-	materialSlotRow.add( materialSlotSelect );
+	const materialSlotSelect = new UISelect().setWidth('170px').setFontSize('12px').onChange(update);
+	materialSlotSelect.setOptions({ 0: '' }).setValue(0);
+	materialSlotRow.add(materialSlotSelect);
 
-	container.add( materialSlotRow );
+	container.add(materialSlotRow);
 
 	// SimulationMaterial type
 
 	const materialClassRow = new UIRow();
-	const materialClass = new UISelect().setWidth( '150px' ).setFontSize( '12px' ).onChange( update );
+	const materialClass = new UISelect().setWidth('150px').setFontSize('12px').onChange(update);
 
-	materialClassRow.add( new UIText( strings.getKey( 'sidebar/material/type' ) ).setWidth( '90px' ) );
-	materialClassRow.add( materialClass );
+	materialClassRow.add(new UIText(strings.getKey('sidebar/material/type')).setWidth('90px'));
+	materialClassRow.add(materialClass);
 
-	container.add( materialClassRow );
+	container.add(materialClassRow);
 
-    // color
+	// color
 
-	const materialColor = SidebarMaterialColorProperty( editor, 'color', strings.getKey( 'sidebar/material/color' ) );
-	container.add( materialColor );
+	const materialColor = SidebarMaterialColorProperty(editor, 'color', strings.getKey('sidebar/material/color'));
+	container.add(materialColor);
 
 	// flatShading
 
-	const materialFlatShading = new SidebarMaterialBooleanProperty( editor, 'flatShading', strings.getKey( 'sidebar/material/flatShading' ) );
-	container.add( materialFlatShading );
+	const materialFlatShading = new SidebarMaterialBooleanProperty(editor, 'flatShading', strings.getKey('sidebar/material/flatShading'));
+	container.add(materialFlatShading);
 
-    // blending
+	// blending
 
 	const materialBlendingOptions = {
 		0: 'No',
@@ -73,41 +73,41 @@ function makeSidebarMaterialOptions(container, editor) {
 		4: 'Multiply',
 	};
 
-	const materialBlending = SidebarMaterialConstantProperty( editor, 'blending', strings.getKey( 'sidebar/material/blending' ), materialBlendingOptions );
-	container.add( materialBlending );
+	const materialBlending = SidebarMaterialConstantProperty(editor, 'blending', strings.getKey('sidebar/material/blending'), materialBlendingOptions);
+	container.add(materialBlending);
 
 	// opacity
 
-	const materialOpacity = SidebarMaterialNumberProperty( editor, 'opacity', strings.getKey( 'sidebar/material/opacity' ), [ 0, 1 ] );
-	container.add( materialOpacity );
+	const materialOpacity = SidebarMaterialNumberProperty(editor, 'opacity', strings.getKey('sidebar/material/opacity'), [0, 1]);
+	container.add(materialOpacity);
 
 	// transparent
 
-	const materialTransparent = SidebarMaterialBooleanProperty( editor, 'transparent', strings.getKey( 'sidebar/material/transparent' ) );
-	container.add( materialTransparent );
-    function update() {
+	const materialTransparent = SidebarMaterialBooleanProperty(editor, 'transparent', strings.getKey('sidebar/material/transparent'));
+	container.add(materialTransparent);
+	function update() {
 
 		const previousSelectedSlot = currentMaterialSlot;
 
-		currentMaterialSlot = parseInt( materialSlotSelect.getValue() );
+		currentMaterialSlot = parseInt(materialSlotSelect.getValue());
 
-		if ( currentMaterialSlot !== previousSelectedSlot ) refreshUI();
+		if (currentMaterialSlot !== previousSelectedSlot) refreshUI();
 
-		let material = editor.getObjectMaterial( currentObject, currentMaterialSlot );
+		let material = editor.getObjectMaterial(currentObject, currentMaterialSlot);
 
-		if( material ){
+		if (material) {
 
-			if ( material.name !== materialClass.getValue() ) {
+			if (material.name !== materialClass.getValue()) {
 
-				material = materials[ materialClass.getValue() ].material;
-	
-				editor.execute( new SetMaterialCommand( editor, currentObject, material, currentMaterialSlot ), 'Zone aplied material: ' + materialClass.getValue() );
-	
+				material = materials[materialClass.getValue()].material;
+
+				editor.execute(new SetMaterialCommand(editor, currentObject, material, currentMaterialSlot), 'Zone aplied material: ' + materialClass.getValue());
+
 				refreshUI();
-	
+
 			}
 		}
-		
+
 
 	}
 
@@ -117,13 +117,13 @@ function makeSidebarMaterialOptions(container, editor) {
 
 		const material = currentObject?.material;
 
-		if ( Array.isArray( material ) ) {
+		if (Array.isArray(material)) {
 
-			materialSlotRow.setDisplay( '' );
+			materialSlotRow.setDisplay('');
 
 		} else {
 
-			materialSlotRow.setDisplay( 'none' );
+			materialSlotRow.setDisplay('none');
 
 		}
 
@@ -131,31 +131,31 @@ function makeSidebarMaterialOptions(container, editor) {
 
 	function refreshUI() {
 
-		if ( ! currentObject ) return;
+		if (!currentObject) return;
 
 		let material = currentObject.material;
 
-		if ( Array.isArray( material ) ) {
+		if (Array.isArray(material)) {
 
 			const slotOptions = {};
 
-			currentMaterialSlot = Math.max( 0, Math.min( material.length, currentMaterialSlot ) );
+			currentMaterialSlot = Math.max(0, Math.min(material.length, currentMaterialSlot));
 
-			for ( let i = 0; i < material.length; i ++ ) {
+			for (let i = 0; i < material.length; i++) {
 
-				slotOptions[ i ] = String( i + 1 ) + ': ' + material[ i ].name;
+				slotOptions[i] = String(i + 1) + ': ' + material[i].name;
 
 			}
 
-			materialSlotSelect.setOptions( slotOptions ).setValue( currentMaterialSlot );
+			materialSlotSelect.setOptions(slotOptions).setValue(currentMaterialSlot);
 
 		}
 
-		material = editor.getObjectMaterial( currentObject, currentMaterialSlot );
+		material = editor.getObjectMaterial(currentObject, currentMaterialSlot);
 
-		materialClass.setOptions( materialOptions );
+		materialClass.setOptions(materialOptions);
 
-		materialClass.setValue( material.name );
+		materialClass.setValue(material.name);
 
 		setRowVisibility();
 
@@ -163,15 +163,15 @@ function makeSidebarMaterialOptions(container, editor) {
 
 	// events
 
-	signals.objectSelected.add( function ( object ) {
+	signals.objectSelected.add(function (object) {
 
 		let hasMaterial = false;
 
-		if ( object && object.material ) {
+		if (object && object.material) {
 
 			hasMaterial = true;
 
-			if ( Array.isArray( object.material ) && object.material.length === 0 ) {
+			if (Array.isArray(object.material) && object.material.length === 0) {
 
 				hasMaterial = false;
 
@@ -179,21 +179,21 @@ function makeSidebarMaterialOptions(container, editor) {
 
 		}
 
-		if ( hasMaterial ) {
+		if (hasMaterial) {
 
 			currentObject = object;
 			refreshUI();
-			container.setDisplay( '' );
+			container.setDisplay('');
 
 		} else {
 
 			currentObject = null;
-			container.setDisplay( 'none' );
+			container.setDisplay('none');
 
 		}
 
-	} );
+	});
 
-	signals.materialChanged.add( refreshUI );
+	signals.materialChanged.add(refreshUI);
 
 }
