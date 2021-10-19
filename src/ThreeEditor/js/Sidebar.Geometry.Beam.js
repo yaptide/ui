03a9619713b98b89fdbd3ef677a7e71ff2ec6f9e
ostatni5 <26521377+ostatni5@users.noreply.github.com>
2021-10-19 10:56:13 +0200

    set minimal energy (#62)

--- src/ThreeEditor/js/Sidebar.Geometry.Beam.js
+++ src/ThreeEditor/js/Sidebar.Geometry.Beam.js
@@ -5,6 +5,34 @@ import { UINumber, UIRow, UIText } from './libs/ui.js';
 
 export function BeamPanel(editor, beam) {
 
+	const container = new UIRow();
+
+	// direction
+
+	const directionRow = new UIRow();
+	const dirX = new UINumber().setPrecision(3).setWidth('50px').onChange(update);
+	const dirY = new UINumber().setPrecision(3).setWidth('50px').onChange(update);
+	const dirZ = new UINumber().setPrecision(3).setWidth('50px').onChange(update);
+
+	directionRow.add(new UIText('Direction ' + editor.unit.name).setWidth('90px'));
+	directionRow.add(dirX, dirY, dirZ);
+	container.add(directionRow);
+
+	// energy
+
+	const energyRow = new UIRow();
+	const energy = new UINumber().setPrecision(3).onChange(update);
+	energy.min = .001;
+	const energyText = new UIText('Energy [MeV]').setWidth('90px');
+
+	energyRow.add(energyText);
+	energyRow.add(energy);
+	container.add(energyRow);
+
+	updateUI();
+
+	// 
+
 	function updateUI() {
 
 		dirX.setValue(beam.direction.x);
@@ -24,35 +52,7 @@ export function BeamPanel(editor, beam) {
 		updateUI();
 	}
 
-
-	const container = new UIRow();
-
-	// direction
-
-	const directionRow = new UIRow();
-	const dirX = new UINumber().setPrecision(3).setWidth('50px').onChange(update);
-	const dirY = new UINumber().setPrecision(3).setWidth('50px').onChange(update);
-	const dirZ = new UINumber().setPrecision(3).setWidth('50px').onChange(update);
-
-	directionRow.add(new UIText('Direction ' + editor.unit.name).setWidth('90px'));
-	directionRow.add(dirX, dirY, dirZ);
-	container.add(directionRow);
-
-	// energy
-
-	const energyRow = new UIRow();
-	const energy = new UINumber().onChange(update);
-	energy.min = 0;
-	const energyText = new UIText('Energy [MeV]').setWidth('90px');
-
-	energyRow.add(energyText);
-	energyRow.add(energy);
-	container.add(energyRow);
-
-
-	updateUI();
-
-	//
+	// signals
 
 	editor.signals.objectChanged.add((object) => {
 
