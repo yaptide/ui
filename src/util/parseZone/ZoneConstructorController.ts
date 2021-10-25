d9f8fcbd19023cb8b4b10d1d49b60bbcfe8898d7
Jakub Niechaj <quban123@gmail.com>
2021-10-25 20:48:04 +0200

    Apply suggestions from code review

--- src/util/parseZone/ZoneConstructorController.ts
+++ src/util/parseZone/ZoneConstructorController.ts
@@ -78,7 +78,7 @@ export default class ZoneConstructorController {
         prefix?: number[] | number,
         tails?: number[][] | number[] | number
     ) {
-        if (prefix)
+        if (Number.isInteger(prefix) || prefix)
             switch (this.getDimension(prefix)) {
                 case 0:
                     this.commonPrefix = [prefix as number];
@@ -91,7 +91,7 @@ export default class ZoneConstructorController {
             }
         else this.commonPrefix = [];
 
-        if (tails)
+        if (Number.isInteger(tails) || tails)
             switch (this.getDimension(tails)) {
                 case 0:
                     this.tails = [[tails as number]];
