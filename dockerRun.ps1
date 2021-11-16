cf18734054ad1bbc2bf2b2de245d12c830d59e91
ostatni5 <26521377+ostatni5@users.noreply.github.com>
2021-11-16 16:22:57 +0100

    deploy (#256)

--- dockerRun.ps1
+++ dockerRun.ps1
@@ -1,1 +1,3 @@
-docker run --name some-nginx -p 8080:80 -v $pwd/build:/usr/share/nginx/html:ro -d nginx
+ls $PWD/build
+docker rm --force yaptide-nginx 
+docker run --name yaptide-nginx -p 8080:80 -v $PWD/build:/usr/share/nginx/html:ro -d nginx
