399492e17f5c88f5d9c008cdd25f661880696aeb
retkiewi <krzret@student.agh.edu.pl>
2021-12-13 00:31:47 +0100

    Deepsource fixes

--- src/WrapperApp/components/ResultsPanel.tsx
+++ src/WrapperApp/components/ResultsPanel.tsx
@@ -1,4 +1,4 @@
-import { Grid, Tab, Tabs, Typography } from '@mui/material';
+import { Grid, Tab, Tabs } from '@mui/material';
 import { Box } from '@mui/system';
 import React, { SyntheticEvent, useState } from 'react';
 import { generateGraphs } from '../../JsRoot/GraphData';
@@ -9,31 +9,36 @@ interface ResultsPanelProps {
 	data?: Object;
 }
 
-const simulationData = (() => {
-	const data = [];
-	for (let index = 0; index < 5; index++) {
-		const numberOfElements = Math.floor(Math.random() * 50) + 100;
-		const obj = {
-			uuid: index,
-			name: `Scoring Mesh${index}`,
-			graphs: Array(numberOfElements)
-				.fill(0)
-				.map((v, i) => {
-					const rand = Math.round(Math.random() * numberOfElements) + 1;
-					return { uuid: i, title: `Mesh${index} - Graph${i} - ${rand}`, value: rand };
-				})
-		};
-		data.push(obj);
-	}
+// const simulationData = (() => {
+// 	const data = [];
+// 	for (let index = 0; index < 5; index++) {
+// 		const numberOfElements = Math.floor(Math.random() * 50) + 100;
+// 		const obj = {
+// 			uuid: index,
+// 			name: `Scoring Mesh${index}`,
+// 			graphs: Array(numberOfElements)
+// 				.fill(0)
+// 				.map((v, i) => {
+// 					const rand = Math.round(Math.random() * numberOfElements) + 1;
+// 					return { uuid: i, title: `Mesh${index} - Graph${i} - ${rand}`, value: rand };
+// 				})
+// 		};
+// 		data.push(obj);
+// 	}
 
-	return data;
-})();
+// 	return data;
+// })();
 
 
 const ResultsPanel = React.memo(
 	function ResultsPanel(props: ResultsPanelProps) {
 		const [tabsValue, setTabsValue] = useState<number>(0);
-
+		
+		/**
+		 * Handles the changing of tabs
+		 * @param event 
+		 * @param newValue 
+		 */
 		const handleChange = (event: SyntheticEvent, newValue: number) => {
 			setTabsValue(newValue);
 		};
@@ -49,7 +54,7 @@ const ResultsPanel = React.memo(
 					{estimators.map((estimator, idx) => {
 						return (
 							<Tab
-								key={estimator.name + idx}
+								key={`tab_${estimator.name}`}
 								label={estimator.name}
 								value={idx}
 							/>
@@ -59,11 +64,11 @@ const ResultsPanel = React.memo(
 				{estimators.map((estimator, idx) => {
 					return (
 						<TabPanel
-							key={"tab_pane_l" + estimator.name + idx}
+							key={`tab_panel_${estimator.name}`}
 							value={tabsValue}
 							index={idx}
 							persistent>
-							<Grid key={"grid_" + estimator.name + idx} container spacing={1}>
+							<Grid key={`grid_${estimator.name}`} container spacing={1}>
 								{generateGraphs(estimator)}
 							</Grid>
 						</TabPanel>
