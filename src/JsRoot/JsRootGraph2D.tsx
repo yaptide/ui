e368d04c3b52a0ae34c6f0f6ec33545d059810ab
Leszek Grzanka <grzanka@agh.edu.pl>
2022-07-11 14:01:38 +0000

    center labels on 2d plots

--- src/JsRoot/JsRootGraph2D.tsx
+++ src/JsRoot/JsRootGraph2D.tsx
@@ -39,6 +39,10 @@ export function JsRootGraph2D(props: Page2D) {
 		histogram.fYaxis.fXmax = y[nypoints - 1];
 		histogram.fYaxis.fTitle = `${props.second_axis.name} [${props.second_axis.unit}]`;
 
+        //centering axes labels
+        histogram.fXaxis.InvertBit(JSROOT.BIT(12))
+        histogram.fYaxis.InvertBit(JSROOT.BIT(12))
+
 		histogram.fTitle = `${props.data.name} [${props.data.unit}]`;
 
 		for (let x = 0; x < nxpoints; x++)
