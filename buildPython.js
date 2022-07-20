46f9f687fe276e3d5c5cd99234a3d59d31e9c252
Leszek Grzanka <grzanka@agh.edu.pl>
2022-07-20 14:49:43 +0000

    improve building python package

--- buildPython.js
+++ buildPython.js
@@ -4,7 +4,6 @@ const { execSync } = require("child_process");
 const { exit } = require("process");
 const commandExists = require('command-exists');
 
-
 const SKIP = process.argv[2] === 'skip';
 
 const destFolder = 'public/libs/converter/dist/';
@@ -81,7 +80,7 @@ const saveFileName = (destFolder, fileName) => {
         });
 
         measureTime('Building yaptide_converter', () => {
-            execSync(`${PYTHON} -m build`, {
+            execSync(`${PYTHON} -m build --wheel --no-isolation`, {
                 cwd: srcFolder
             })
         });
@@ -111,9 +110,3 @@ const saveFileName = (destFolder, fileName) => {
         saveFileName(destFolder, buildFileName);
     }
 })();
-
-
-
-
-
-
