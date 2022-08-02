3eb09a825454cc4287dadbacfaebebd39fe39aa9
ostatni5 <26521377+ostatni5@users.noreply.github.com>
2022-08-02 12:58:44 +0200

    deepsource

--- src/util/csv/Csv.ts
+++ src/util/csv/Csv.ts
@@ -6,8 +6,8 @@ export const commentCsvString = (string: string) => {
 
 export const estimatorPageToCsv = (estimator: Estimator, page: Page1D) => {
 
-    let estimatorMetaData = JSON.stringify(estimator.metadata ?? {}, null, 4);
-    let pageMetaData = JSON.stringify(page.metadata ?? {}, null, 4);
+    const estimatorMetaData = JSON.stringify(estimator.metadata ?? {}, null, 4);
+    const pageMetaData = JSON.stringify(page.metadata ?? {}, null, 4);
 
     const titleXaxis = `${page.first_axis.name} [${page.first_axis.unit}]`;
     const titleYaxis = `${page.data.name} [${page.data.unit}]`;
