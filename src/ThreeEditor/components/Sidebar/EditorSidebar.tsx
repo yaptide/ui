7815d875bebf0dfeec9fbb4c6124e1fcfdce947a
Szymon Kania <26521377+ostatni5@users.noreply.github.com>
2023-03-08 17:17:52 +0100

    add physics settings (#528) (#805)
    * add physics settings (#528)
    
    * rename straggle to straggling

--- src/ThreeEditor/components/Sidebar/EditorSidebar.tsx
+++ src/ThreeEditor/components/Sidebar/EditorSidebar.tsx
@@ -12,11 +12,15 @@ import { AddDetectGeometryCommand } from '../../js/commands/AddDetectGeometryCom
 import { AddFilterCommand } from '../../js/commands/AddFilterCommand';
 import { AddOutputCommand } from '../../js/commands/AddOutputCommand';
 import { Context } from '../../js/Editor.Context';
+import { Stack } from '@mui/material';
+import { Typography } from '@mui/material';
+import { Divider } from '@mui/material';
+import { PhysicConfiguration } from './propeteries/category/PhysicConfiguration';
 
 export function EditorSidebar(props: { editor: Editor }) {
 	const { editor } = props;
 
-	const [value, setValue] = useState('Geometry');
+	const [selectedTab, setValue] = useState('Geometry');
 
 	const handleChange = (_event: React.SyntheticEvent, newValue: string) => {
 		switch (newValue) {
@@ -138,24 +142,54 @@ export function EditorSidebar(props: { editor: Editor }) {
 	return (
 		<Box sx={{ height: '100vh', overflowY: 'auto' }}>
 			<Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-				<Tabs value={value} onChange={handleChange} aria-label='basic tabs example'>
+				<Tabs value={selectedTab} onChange={handleChange} aria-label='basic tabs example'>
 					<Tab label='Geometry' value={'Geometry'} />
 					<Tab label='Scoring' value={'Scoring'} />
 					<Tab label='Settings' value={'Settings'} />
 				</Tabs>
 			</Box>
-			<TabPanel value={value} index={'Geometry'} persistentIfVisited>
+			<TabPanel value={selectedTab} index={'Geometry'} persistentIfVisited>
 				<EditorSidebarTabTree elements={geometryTabElements}></EditorSidebarTabTree>
 			</TabPanel>
-			<TabPanel value={value} index={'Scoring'} persistentIfVisited>
+			<TabPanel value={selectedTab} index={'Scoring'} persistentIfVisited>
 				<EditorSidebarTabTree elements={scoringTabElements}></EditorSidebarTabTree>
 			</TabPanel>
-			<TabPanel value={value} index={'Settings'} persistentIfVisited></TabPanel>
-			<PropertiesPanel
-				editor={editor}
-				boxProps={{
-					sx: { marginTop: '1rem', padding: '.5rem', overflowY: 'auto' }
-				}}></PropertiesPanel>
+
+			<TabPanel
+				customCss={{ background: 'none' }}
+				value={selectedTab}
+				index={'Settings'}
+				persistentIfVisited>
+				<Stack sx={{ padding: '.5rem' }} spacing={2}>
+					<Box>
+						<Typography variant='h6' sx={{ margin: '0.5rem 0' }}>
+							Beam
+						</Typography>
+						<PropertiesPanel
+							editor={editor}
+							boxProps={{
+								sx: { marginTop: '.5rem', overflowY: 'auto' }
+							}}
+						/>
+					</Box>
+					<Divider light />
+					<Box>
+						<Typography variant='h6' sx={{ margin: '0.5rem 0' }}>
+							Physics
+						</Typography>
+						<PhysicConfiguration editor={editor} object={editor.physic} />
+					</Box>
+				</Stack>
+			</TabPanel>
+
+			{selectedTab !== 'Settings' && (
+				<PropertiesPanel
+					editor={editor}
+					boxProps={{
+						sx: { marginTop: '1rem', padding: '0 .5rem', overflowY: 'auto' }
+					}}
+				/>
+			)}
 		</Box>
 	);
 }
