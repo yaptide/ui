7a0c91e0bc749cd3f4cb3f0cf78f305e8da03f67
Mateusz Wronka <matwro2001@gmail.com>
2023-05-12 13:32:18 +0200

    test mostly working - checking one text field for now

--- src/__tests__/Converter.test.ts
+++ src/__tests__/Converter.test.ts
@@ -17,24 +17,54 @@ describe('NavDrawer component', () => {
   }, 30000);
 
   //this test checks if converter works correctly - opens an example and generates config files
-  test('closes and opens the drawer on click', async () => {
+  test('converter generates correct files', async () => {
     await driver.get('http://localhost:3000');
 
     //find the "Editor" button on the left menu and click it to open the editor
-    const editorButton = await driver.findElement(By.xpath("/html/body/div/div/div[1]/div/ul[1]/li[1]/div"));
+    const editorButton = await driver.findElement(By.css("li.MuiListItem-root:nth-child(3)"));
     await editorButton.click();
 
     //find the "open" button (second from the left on the upper bar) and then click it
-    const openButton = await driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div/div/header/div/div[2]/span"));
+    const openButton = await driver.findElement(By.css("div.css-12z0wuy:nth-child(2) > span:nth-child(1)"));
     await openButton.click();
     
     //wait for the "open project" window to appear
-    await driver.wait(until.elementLocated(By.className("MuiPaper-root MuiPaper-elevation MuiPaper-rounded MuiPaper-elevation24 MuiDialog-paper MuiDialog-paperScrollPaper MuiDialog-paperWidthSm css-1qxadfk-MuiPaper-root-MuiDialog-paper")), 10000);
+    await driver.wait(until.elementLocated(By.css(".MuiPaper-elevation24")), 10000);
 
     //find the "examples" button in the window and click it. we need to check if xpath contains 'T-0', as the id of the button changes every time the window appears
     const examplesButton = await driver.findElement(By.xpath("// *[contains(@id,'T-0')]"));
     await examplesButton.click();
-    
-    await new Promise(resolve => setTimeout(resolve, 5000));
+
+    //find the first example option and click it
+    const firstExample = await driver.findElement(By.css("li.MuiListItem-root:nth-child(1) > div:nth-child(1)"));
+    await firstExample.click();
+
+    //find the "load" button and click it
+    const loadButton = await driver.findElement(By.css(".MuiButton-contained"));
+    await loadButton.click();
+
+    //accept the "current data will be lost" alert
+    driver.switchTo().alert().accept();
+
+    await new Promise(resolve => setTimeout(resolve, 1500));
+
+    //find the "Input Files" button on the left menu and click it to open the editor
+    const filesButton = await driver.findElement(By.css("li.MuiListItem-root:nth-child(4)"));
+    await filesButton.click();
+
+    await new Promise(resolve => setTimeout(resolve, 7500));
+
+    //wait until the "generate from editor" button and click it (it takes some time for the button to change from "initializing")
+    //classname is used as again the id changes every time
+    await driver.wait(until.elementLocated(By.className("MuiButtonBase-root MuiButton-root MuiLoadingButton-root MuiButton-contained MuiButton-containedInfo MuiButton-sizeMedium MuiButton-containedSizeMedium MuiButton-root MuiLoadingButton-root MuiButton-contained MuiButton-containedInfo MuiButton-sizeMedium MuiButton-containedSizeMedium css-iqndx8-MuiButtonBase-root-MuiButton-root-MuiLoadingButton-root")), 20000);
+    const generateButton = await driver.findElement(By.className("MuiButtonBase-root MuiButton-root MuiLoadingButton-root MuiButton-contained MuiButton-containedInfo MuiButton-sizeMedium MuiButton-containedSizeMedium MuiButton-root MuiLoadingButton-root MuiButton-contained MuiButton-containedInfo MuiButton-sizeMedium MuiButton-containedSizeMedium css-iqndx8-MuiButtonBase-root-MuiButton-root-MuiLoadingButton-root"));
+    await generateButton.click();
+  
+    //expect (await driver.getPageSource()).toContain("{'version': 'unknown', 'label': 'development', 'simulator': 'shieldhit'}");
+
+    //find all the text fields and check if they contain the correct text
+    const infoText = await driver.findElement(By.css('.MuiCardContent-root > div:nth-child(1) > div:nth-child(2) > textarea:nth-child(1)')).getText();
+    expect (infoText).toContain("{'version': 'unknown', 'label': 'development', 'simulator': 'shieldhit'}");
+
   }, 30000);
 });
