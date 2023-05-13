fe23980ce215e416aa76cabf5c3fd2681bb8b81e
Mateusz Wronka <matwro2001@gmail.com>
2023-05-13 17:59:46 +0200

    select buttons by text. look for elements inside the popup window instead of the entire page. regex kept in constant. removed some unnecessary lines and imports

--- src/__tests__/Converter.test.ts
+++ src/__tests__/Converter.test.ts
@@ -1,7 +1,6 @@
 import { Builder, By, WebDriver, until } from 'selenium-webdriver';
 import chrome from 'selenium-webdriver/chrome';
-import { readFileSync, writeFileSync } from 'fs';
-import exp from 'constants';
+import { readFileSync } from 'fs';
 
 describe('NavDrawer component', () => {
   let driver: WebDriver;
@@ -31,24 +30,22 @@ describe('NavDrawer component', () => {
     await openButton.click();
     
     //wait for the "open project" window to appear
-    await driver.wait(until.elementLocated(By.css(".MuiPaper-elevation24")), 10000);
+    const openProjectWidget = await driver.wait(until.elementLocated(By.css(".MuiPaper-elevation24")), 10000);
 
-    //find the "examples" button in the window and click it. we need to check if xpath contains 'T-0', as the id of the button changes every time the window appears
-    const examplesButton = await driver.findElement(By.xpath("// *[contains(@id,'T-0')]"));
+    //find the "examples" button in the window and click it. (we find it by contained text as its id changes every time)
+    const examplesButton = await openProjectWidget.findElement(By.xpath("//button[contains(text(),'Examples')]"));
     await examplesButton.click();
 
     //find the first example option and click it
-    const firstExample = await driver.findElement(By.css("li.MuiListItem-root:nth-child(1) > div:nth-child(1)"));
+    const firstExample = await openProjectWidget.findElement(By.css("li.MuiListItem-root:nth-child(1) > div:nth-child(1)"));
     await firstExample.click();
 
     //find the "load" button and click it
-    const loadButton = await driver.findElement(By.css(".MuiButton-contained"));
+    const loadButton = await openProjectWidget.findElement(By.css(".MuiButton-contained"));
     await loadButton.click();
 
     //accept the "current data will be lost" alert
-    driver.switchTo().alert().accept();
-
-    await new Promise(resolve => setTimeout(resolve, 1500));
+    await driver.switchTo().alert().accept();
 
     //find the "Input Files" button on the left menu and click it to open the editor
     const filesButton = await driver.findElement(By.css("li.MuiListItem-root:nth-child(4)"));
@@ -56,7 +53,7 @@ describe('NavDrawer component', () => {
 
     //wait until the "generate from editor" button and click it (it takes some time for the button to change from "initializing")
     //xpath is used as again the id changes every time
-    const generateButton = await driver.findElement(By.xpath("/html/body/div/div/div[3]/div/div/div[1]/button"));
+    const generateButton = await driver.findElement(By.xpath("//button[contains(text(),'Generate from Editor')]"));
     await driver.wait(until.elementIsEnabled(generateButton), 15000);
     await generateButton.click();
 
@@ -65,23 +62,25 @@ describe('NavDrawer component', () => {
 
     //find all the text fields and check if they contain the correct text. characters other than text, numbers and dots are removed from the text before comparison
     //also check if expected text loaded from file is not empty
-    const geoText = (await driver.findElement(By.css('.MuiCardContent-root > div:nth-child(2) > div:nth-child(2) > textarea:nth-child(1)')).getText()).replace(/[^a-zA-Z0-9.]/g, '');
-    const expectedGeoText = readFileSync('src/libs/converter/input_examples/expected_shieldhit_output/geo.dat', 'utf-8').replace(/[^a-zA-Z0-9.]/g, '');
+    const regex = /[^a-zA-Z0-9.]/g;
+
+    const geoText = (await driver.findElement(By.css('.MuiCardContent-root > div:nth-child(2) > div:nth-child(2) > textarea:nth-child(1)')).getText()).replace(regex, '');
+    const expectedGeoText = readFileSync('src/libs/converter/input_examples/expected_shieldhit_output/geo.dat', 'utf-8').replace(regex, '');
     expect (expectedGeoText).not.toBe("");
     expect (geoText).toContain(expectedGeoText);
 
-    const matText = (await driver.findElement(By.css('.MuiCardContent-root > div:nth-child(3) > div:nth-child(2) > textarea:nth-child(1)')).getText()).replace(/[^a-zA-Z0-9.]/g, '');
-    const expectedMatText = readFileSync('src/libs/converter/input_examples/expected_shieldhit_output/mat.dat', 'utf-8').replace(/[^a-zA-Z0-9.]/g, '');
+    const matText = (await driver.findElement(By.css('.MuiCardContent-root > div:nth-child(3) > div:nth-child(2) > textarea:nth-child(1)')).getText()).replace(regex, '');
+    const expectedMatText = readFileSync('src/libs/converter/input_examples/expected_shieldhit_output/mat.dat', 'utf-8').replace(regex, '');
     expect (expectedMatText).not.toBe("");
     expect (matText).toContain(expectedMatText);
 
-    const beamText = (await driver.findElement(By.css('.MuiCardContent-root > div:nth-child(4) > div:nth-child(2) > textarea:nth-child(1)')).getText()).replace(/[^a-zA-Z0-9.]/g, '');
-    const expectedBeamText = readFileSync('src/libs/converter/input_examples/expected_shieldhit_output/beam.dat', 'utf-8').replace(/[^a-zA-Z0-9.]/g, '');
+    const beamText = (await driver.findElement(By.css('.MuiCardContent-root > div:nth-child(4) > div:nth-child(2) > textarea:nth-child(1)')).getText()).replace(regex, '');
+    const expectedBeamText = readFileSync('src/libs/converter/input_examples/expected_shieldhit_output/beam.dat', 'utf-8').replace(regex, '');
     expect (expectedBeamText).not.toBe("");
     expect (beamText).toContain(expectedBeamText);
     
-    const detectText = (await driver.findElement(By.css('.MuiCardContent-root > div:nth-child(5) > div:nth-child(2) > textarea:nth-child(1)')).getText()).replace(/[^a-zA-Z0-9.]/g, '');
-    const expectedDetectText = readFileSync('src/libs/converter/input_examples/expected_shieldhit_output/detect.dat', 'utf-8').replace(/[^a-zA-Z0-9.]/g, '');
+    const detectText = (await driver.findElement(By.css('.MuiCardContent-root > div:nth-child(5) > div:nth-child(2) > textarea:nth-child(1)')).getText()).replace(regex, '');
+    const expectedDetectText = readFileSync('src/libs/converter/input_examples/expected_shieldhit_output/detect.dat', 'utf-8').replace(regex, '');
     expect (expectedDetectText).not.toBe("");
     expect (detectText).toContain(expectedDetectText);
 
