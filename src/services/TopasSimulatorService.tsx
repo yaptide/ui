bc4134ea6df82fec2ff7622cd7ac08aa7f965be2
Mateusz Wronka <matwro2001@gmail.com>
2023-05-17 18:50:18 +0200

    fixed imports in topas converter

--- src/services/TopasSimulatorService.tsx
+++ src/services/TopasSimulatorService.tsx
@@ -1,8 +1,8 @@
 import { ReactNode, useCallback, useRef } from 'react';
 import { Estimator } from '../JsRoot/GraphData';
 import { EditorJson } from '../ThreeEditor/js/EditorJson';
-import { FilterJSON } from '../ThreeEditor/util/Detect/DetectFilter';
-import { ScoringManagerJSON } from '../ThreeEditor/util/Scoring/ScoringManager';
+import { FilterJSON } from '../ThreeEditor/Simulation/Scoring/DetectFilter';
+import { ScoringManagerJSON } from '../ThreeEditor/Simulation/Scoring/ScoringManager';
 import {
 	RequestCancelJob,
 	RequestGetJobStatus,
