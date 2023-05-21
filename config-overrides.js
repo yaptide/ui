278d065c016320957df654453731d38b07da07bf
Jakub Niechaj <quban123@gmail.com>
2023-05-21 12:39:47 +0200

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,5 @@
 module.exports = function override(webpackConfig) {
+	// allows create-react-app to load modules with `.cjs` and `.mjs` extension types
 	webpackConfig.module.rules.push({
 		test: /\.(c|m)js$/,
 		include: /node_modules/,
