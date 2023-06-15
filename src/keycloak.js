789da67e1d2d586abe3c6e29cd4ac2bc5b8d3a6e
Dominik Hendzel <128628266+hendzeld@users.noreply.github.com>
2023-06-15 23:30:27 +0200

    Print token for debug purposes

--- src/keycloak.js
+++ src/keycloak.js
@@ -7,6 +7,13 @@ const keycloakConfig = {
 };
 
 const keycloak = new Keycloak(keycloakConfig);
-keycloak.init();
+keycloak.init({
+  onLoad: 'login-required',
+  promiseType: 'native'
+}).then(auth => {
+  if(auth) {
+  console.log("token", keycloak.token);
+  }
+});
 
 export default keycloak;
