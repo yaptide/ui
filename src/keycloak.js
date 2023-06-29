dd4b8cb9267046ec027d0fefe93afe64aea10513
Leszek Grzanka <leszek.grzanka@gmail.com>
2023-06-29 19:41:57 +0200

    hardcoding

--- src/keycloak.js
+++ src/keycloak.js
@@ -1,9 +1,9 @@
 import Keycloak from 'keycloak-js';
 
 const keycloakConfig = {
-	url: 'http://localhost:8080',
-	realm: 'myrealm',
-	clientId: 'myclient'
+	url: 'https://sso.pre.plgrid.pl/auth/',
+	realm: 'PLGrid',
+	clientId: 'yaptide-staging'
 };
 
 const keycloak = new Keycloak(keycloakConfig);
