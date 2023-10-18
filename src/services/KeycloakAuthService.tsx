44d59cfed7bf586ae043eaa3e4ec05adfdd48065
Jakub Niechaj <quban123@gmail.com>
2023-10-18 19:51:59 +0200

    Update process env

--- src/services/KeycloakAuthService.tsx
+++ src/services/KeycloakAuthService.tsx
@@ -4,9 +4,13 @@ import { KeycloakProvider } from 'keycloak-react-web';
 import { GenericContextProviderProps } from './GenericContext';
 
 const authInstance = new Keycloak({
-	url: process.env.KEYCLOAK_AUTH_URL ?? 'https://sso.pre.plgrid.pl/auth/',
-	realm: process.env.KEYCLOAK_REALM ?? 'PLGrid',
-	clientId: process.env.KEYCLOAK_CLIENT ?? 'yaptide-staging'
+	url: `${
+		process.env.KEYCLOAK_BASE_URL
+			? process.env.KEYCLOAK_BASE_URL + '/auth/'
+			: 'https://localhost:8080/auth/'
+	}`,
+	realm: `${process.env.KEYCLOAK_REALM ?? ''}`,
+	clientId: `${process.env.KEYCLOAK_CLIENT_ID ?? ''}`
 }) as any;
 
 const initOptions = {
