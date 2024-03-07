3f0f955652e2feaf13d0998619fac82184784e57
Leszek Grzanka <grzanka@agh.edu.pl>
2024-03-07 14:39:14 +0100

    Update src/ThreeEditor/components/Editor/EditorAppBar/components/EditorTitlebar.tsx

--- src/ThreeEditor/components/Editor/EditorAppBar/components/EditorTitlebar.tsx
+++ src/ThreeEditor/components/Editor/EditorAppBar/components/EditorTitlebar.tsx
@@ -21,7 +21,6 @@ export function EditorTitleBar() {
 	const [editMode, setEditMode] = useState(false);
 	const [title, setTitle] = useState<string>(yaptideEditor?.config.getKey('project/title'));
 	const titleRef = useRef(title);
-	// useDocumentTitle(titleRef.current);
 
 	const startSave = useCallback(() => {
 		setSaving(true);
