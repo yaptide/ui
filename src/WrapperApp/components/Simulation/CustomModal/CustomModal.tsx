61777df2a3ff17e6bc96529da9d7b33d0c6c3d9f
matuszsmig <smigala@student.agh.edu.pl>
2024-10-16 21:10:25 +0200

    use customdialog in custom modal

--- src/WrapperApp/components/Simulation/CustomModal/CustomModal.tsx
+++ src/WrapperApp/components/Simulation/CustomModal/CustomModal.tsx
@@ -1,6 +1,15 @@
 import CloseIcon from '@mui/icons-material/Close';
-import { Box, Button, IconButton, Modal, Typography } from '@mui/material';
+import {
+	Button,
+	DialogActions,
+	DialogContent,
+	DialogContentText,
+	DialogTitle,
+	IconButton
+} from '@mui/material';
 import React from 'react';
+
+import { CustomDialog } from '../../../../ThreeEditor/components/Dialog/CustomDialog';
 
 interface CustomModalProps {
 	open: boolean;
@@ -12,23 +21,12 @@ const CustomModal = ({ open, setOpen, onConfirm }: CustomModalProps) => {
 	const handleClose = () => setOpen(false);
 
 	return (
-		<Modal
+		<CustomDialog
 			open={open}
 			onClose={handleClose}
-			aria-labelledby='modal-title'
-			aria-describedby='modal-description'>
-			<Box
-				sx={{
-					position: 'absolute',
-					top: '50%',
-					left: '50%',
-					transform: 'translate(-50%, -50%)',
-					width: 400,
-					bgcolor: 'background.paper',
-					border: '2px solid #000',
-					boxShadow: 24,
-					p: 4
-				}}>
+			title='Delete Simulation'
+			contentText='Are you sure to delete this simulation?'>
+			<DialogTitle>
 				<IconButton
 					aria-label='close'
 					onClick={handleClose}
@@ -40,29 +38,22 @@ const CustomModal = ({ open, setOpen, onConfirm }: CustomModalProps) => {
 					}}>
 					<CloseIcon />
 				</IconButton>
-				<Typography
-					id='modal-title'
-					variant='h6'
-					component='h2'
-					sx={{ textAlign: 'center' }}>
-					Are you sure to delete this simulation?
-				</Typography>
-				<Box sx={{ display: 'flex', justifyContent: 'space-around', mt: 4 }}>
-					<Button
-						variant='contained'
-						color='primary'
-						onClick={onConfirm}>
-						Yes
-					</Button>
-					<Button
-						variant='contained'
-						color='secondary'
-						onClick={handleClose}>
-						No
-					</Button>
-				</Box>
-			</Box>
-		</Modal>
+			</DialogTitle>
+			<DialogActions sx={{ display: 'flex', justifyContent: 'space-between' }}>
+				<Button
+					variant='contained'
+					color='primary'
+					onClick={onConfirm}>
+					Yes
+				</Button>
+				<Button
+					variant='contained'
+					color='secondary'
+					onClick={handleClose}>
+					No
+				</Button>
+			</DialogActions>
+		</CustomDialog>
 	);
 };
 
