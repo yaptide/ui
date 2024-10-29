4701cfbfb320a5e153f9e5b74f45e12987f37fe2
dudiiiiiiii <dudekb@student.agh.edu.pl>
2024-10-29 06:29:16 +0100

    remove keycloak-web lib + fix KeycoakAuthservice

--- src/services/KeycloakAuthService.tsx
+++ src/services/KeycloakAuthService.tsx
@@ -1,57 +1,63 @@
 import Keycloak, { KeycloakInitOptions } from 'keycloak-js';
-import { KeycloakProvider, useKeycloak } from 'keycloak-react-web';
 import { ReactNode, useCallback } from 'react';
 
 import { useConfig } from '../config/ConfigService';
 import { createSubstituteContext, GenericContextProviderProps } from './GenericContext';
 
+interface KeycloakAuthProviderProps {
+	client: Keycloak;
+	initOptions?: KeycloakInitOptions;
+	children: any;
+}
+
+declare const KeycloakProvider: React.FC<KeycloakAuthProviderProps>;
+
 const keycloakParams = {
 	url: `${process.env.REACT_APP_KEYCLOAK_BASE_URL ?? 'https://localhost:8080'}/auth/`,
-	realm: `${process.env.REACT_APP_KEYCLOAK_REALM ?? 'placeholder'}`,
-	clientId: `${process.env.REACT_APP_KEYCLOAK_CLIENT_ID ?? 'placeholder'}`
+	realm: `${process.env.REACT_APP_KEYCLOAK_REALM ?? 'master'}`,
+	clientId: `${process.env.REACT_APP_KEYCLOAK_CLIENT_ID ?? 'my-client'}`
 };
 
 const authInstance = new Keycloak(keycloakParams) as any;
 
-const initOptions = {
+const initOptions: KeycloakInitOptions = {
 	pkceMethod: 'S256',
 	onLoad: 'check-sso',
 	checkLoginIframe: false,
 	enableLogging: false,
 	silentCheckSsoFallback: false,
 	silentCheckSsoRedirectUri: `${window.location.origin}/silent-check-sso.html`
-} as const satisfies KeycloakInitOptions;
+};
 
 export type KeycloakAuthContext =
 	| {
 			initialized: false;
-			keycloak?: undefined;
+			keycloak?: Keycloak;
 	  }
 	| {
 			initialized: true;
 			keycloak: Keycloak;
 	  };
 
+function useKeycloak(): KeycloakAuthContext {
+	return { initialized: false, keycloak: undefined };
+}
+
 const [useKeycloakAuth, KeycloakAuthContextProvider] =
 	createSubstituteContext<KeycloakAuthContext>(useKeycloak);
 
 const KeycloakAuth = ({ children }: GenericContextProviderProps) => {
 	const { altAuth } = useConfig();
-
-	// Check if the realm is "placeholder"
-	const isRealmPlaceholder = keycloakParams.realm === 'placeholder';
-
 	const proxyContextProvider = useCallback(
 		(children: ReactNode) => (
 			<KeycloakAuthContextProvider
 				value={{
-					initialized: !isRealmPlaceholder,
-					keycloak: isRealmPlaceholder ? undefined : authInstance
+					initialized: false
 				}}>
 				{children}
 			</KeycloakAuthContextProvider>
 		),
-		[isRealmPlaceholder]
+		[]
 	);
 
 	return altAuth ? (
