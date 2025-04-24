78172dcf7880d9150201a3e243df28a9784ee92a
Leszek Grzanka <leszek.grzanka@gmail.com>
2025-04-24 11:37:57 +0200

    build(deps): fix poetry installation syntax in buildPython.js

--- buildPython.js
+++ buildPython.js
@@ -90,9 +90,7 @@ const saveFileName = (destFolder, fileName) => {
 		});
 
 		measureTime('Installing build module for python', () => {
-			executeCommand(
-				`${venvCommandPrefix} pip install "poetry == 1.8.5" ${venvCommandSuffix}`
-			);
+			executeCommand(`${venvCommandPrefix} pip install "poetry==1.8.5" ${venvCommandSuffix}`);
 		});
 
 		measureTime('Building yaptide_converter', () => {
