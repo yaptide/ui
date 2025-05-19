2e6f9fce3aa8c68678789366d4b612326c045b1b
Konrad Michalik <kmichalikk19@gmail.com>
2025-05-19 22:45:37 +0200

    add outputs tab for Geant

--- src/ThreeEditor/components/Sidebar/properties/category/GeantOutputConfiguration.tsx
+++ src/ThreeEditor/components/Sidebar/properties/category/GeantOutputConfiguration.tsx
@@ -1,94 +1,41 @@
-import { Button, Grid, ToggleButton, ToggleButtonGroup } from '@mui/material';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback } from 'react';
 import { Object3D } from 'three';
 
-import { useDialog } from '../../../../../services/DialogService';
 import { useSmartWatchEditorState } from '../../../../../util/hooks/signals';
-import { AddQuantityCommand } from '../../../../js/commands/AddQuantityCommand';
 import { SetOutputSettingsCommand } from '../../../../js/commands/SetOutputSettingsCommand';
 import { YaptideEditor } from '../../../../js/YaptideEditor';
 import {
-	CommonScoringOutput,
-	isCommonOutput
-} from '../../../../Simulation/Scoring/CommonScoringOutput';
-import { GeantScoringOutput } from '../../../../Simulation/Scoring/GeantScoringOutput';
-import { SCORING_TYPE_ENUM } from '../../../../Simulation/Scoring/ScoringOutputTypes';
+	GeantScoringOutput,
+	isGeantOutput
+} from '../../../../Simulation/Scoring/GeantScoringOutput';
 import {
 	ObjectSelectOptionType,
 	ObjectSelectPropertyField
 } from '../fields/ObjectSelectPropertyField';
-import { PropertyField } from '../fields/PropertyField';
 import { PropertiesCategory } from './PropertiesCategory';
 
 export function GeantOutputConfiguration(props: { editor: YaptideEditor; object: Object3D }) {
 	const { object, editor } = props;
-	const { open: changeScoringType } = useDialog('changeScoringType');
 	const { state: watchedObject } = useSmartWatchEditorState(
 		editor,
 		object as unknown as GeantScoringOutput
 	);
-	//
-	// const [scoringType, setScoringType] = useState(
-	// 	watchedObject.scoringType ?? SCORING_TYPE_ENUM.DETECTOR
-	// );
-	//
-	// useEffect(() => {
-	// 	// needed to properly set the scoring type, useState above is not enough
-	// 	setScoringType(watchedObject.scoringType ?? SCORING_TYPE_ENUM.DETECTOR);
-	// }, [watchedObject.scoringType]);
-	//
-	// const handleChangedDetector = useCallback(
-	// 	(v: ObjectSelectOptionType) => {
-	// 		editor.execute(
-	// 			new SetOutputSettingsCommand(
-	// 				editor,
-	// 				watchedObject.object,
-	// 				'detector',
-	// 				editor.detectorManager.getDetectorByUuid(v.uuid)
-	// 			)
-	// 		);
-	// 	},
-	// 	[editor, watchedObject]
-	// );
-	//
-	// const handleChangedZone = useCallback(
-	// 	(v: ObjectSelectOptionType) => {
-	// 		editor.execute(
-	// 			new SetOutputSettingsCommand(
-	// 				editor,
-	// 				watchedObject.object,
-	// 				'zone',
-	// 				editor.zoneManager.getZoneByUuid(v.uuid)
-	// 			)
-	// 		);
-	// 	},
-	// 	[editor, watchedObject]
-	// );
-	//
-	// const handleChangeOutputType = (
-	// 	event: React.MouseEvent<HTMLElement>,
-	// 	newAlignment: SCORING_TYPE_ENUM
-	// ) => {
-	// 	if (watchedObject.quantities.length > 0) {
-	// 		changeScoringType({
-	// 			yaptideEditor: editor,
-	// 			scoringOutput: watchedObject,
-	// 			newAlignment: newAlignment,
-	// 			setScoringType: setScoringType
-	// 		});
-	// 		setScoringType(scoringType); // required for toggle component to work properly
-	// 	} else {
-	// 		if (newAlignment === SCORING_TYPE_ENUM.ZONE) {
-	// 			setScoringType(SCORING_TYPE_ENUM.ZONE);
-	// 			watchedObject.scoringType = SCORING_TYPE_ENUM.ZONE;
-	// 		} else if (newAlignment === SCORING_TYPE_ENUM.DETECTOR) {
-	// 			setScoringType(SCORING_TYPE_ENUM.DETECTOR);
-	// 			watchedObject.scoringType = SCORING_TYPE_ENUM.DETECTOR;
-	// 		}
-	// 	}
-	// };
 
-	const visibleFlag = isCommonOutput(watchedObject);
+	const handleChangedDetector = useCallback(
+		(v: ObjectSelectOptionType) => {
+			editor.execute(
+				new SetOutputSettingsCommand(
+					editor,
+					watchedObject.object,
+					'detector',
+					editor.detectorManager.getDetectorByUuid(v.uuid)
+				)
+			);
+		},
+		[editor, watchedObject]
+	);
+
+	const visibleFlag = isGeantOutput(watchedObject);
 
 	return (
 		<PropertiesCategory
@@ -96,65 +43,17 @@ export function GeantOutputConfiguration(props: { editor: YaptideEditor; object:
 			visible={visibleFlag}>
 			{visibleFlag && (
 				<>
-					{/*<PropertyField*/}
-					{/*	label='Scoring Type'*/}
-					{/*	disabled={false}>*/}
-					{/*	<ToggleButtonGroup*/}
-					{/*		value={scoringType}*/}
-					{/*		exclusive*/}
-					{/*		onChange={handleChangeOutputType}*/}
-					{/*		aria-label='scoring type'*/}
-					{/*		size='small'>*/}
-					{/*		<ToggleButton*/}
-					{/*			value={SCORING_TYPE_ENUM.DETECTOR}*/}
-					{/*			disabled={scoringType === SCORING_TYPE_ENUM.DETECTOR}*/}
-					{/*			aria-label='left aligned'>*/}
-					{/*			Detector*/}
-					{/*		</ToggleButton>*/}
-
-					{/*		<ToggleButton*/}
-					{/*			value={SCORING_TYPE_ENUM.ZONE}*/}
-					{/*			disabled={scoringType === SCORING_TYPE_ENUM.ZONE}*/}
-					{/*			aria-label='left aligned'>*/}
-					{/*			Zone*/}
-					{/*		</ToggleButton>*/}
-					{/*	</ToggleButtonGroup>*/}
-					{/*</PropertyField>*/}
-
-					{/*{scoringType === SCORING_TYPE_ENUM.DETECTOR && (*/}
-					{/*	<ObjectSelectPropertyField*/}
-					{/*		label='Detector'*/}
-					{/*		value={watchedObject.detector?.uuid ?? ''}*/}
-					{/*		options={editor.detectorManager.getDetectorOptions(value => {*/}
-					{/*			return (*/}
-					{/*				!editor.scoringManager*/}
-					{/*					.getTakenDetectors()*/}
-					{/*					.includes(value.uuid) ||*/}
-					{/*				value.uuid === watchedObject.detector?.uuid*/}
-					{/*			);*/}
-					{/*		})}*/}
-					{/*		onChange={handleChangedDetector}*/}
-					{/*	/>*/}
-					{/*)}*/}
-
-					{/*{scoringType === SCORING_TYPE_ENUM.ZONE && (*/}
-					{/*	<ObjectSelectPropertyField*/}
-					{/*		label='Zone'*/}
-					{/*		value={watchedObject.zone?.uuid ?? ''}*/}
-					{/*		options={editor.zoneManager.getZoneOptionsForScoring()}*/}
-					{/*		onChange={handleChangedZone}*/}
-					{/*	/>*/}
-					{/*)}*/}
-					{/*<Grid size={12}>*/}
-					{/*	<Button*/}
-					{/*		sx={{ width: '100%' }}*/}
-					{/*		variant='contained'*/}
-					{/*		onClick={() =>*/}
-					{/*			editor.execute(new AddQuantityCommand(editor, watchedObject.object))*/}
-					{/*		}>*/}
-					{/*		Add new quantity*/}
-					{/*	</Button>*/}
-					{/*</Grid>*/}
+					<ObjectSelectPropertyField
+						label='Detector'
+						value={watchedObject.detector?.uuid ?? ''}
+						options={editor.detectorManager.getDetectorOptions(value => {
+							return (
+								!editor.scoringManager.getTakenDetectors().includes(value.uuid) ||
+								value.uuid === watchedObject.detector?.uuid
+							);
+						})}
+						onChange={handleChangedDetector}
+					/>
 				</>
 			)}
 		</PropertiesCategory>
