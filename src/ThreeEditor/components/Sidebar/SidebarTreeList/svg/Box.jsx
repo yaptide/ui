093afd577f0423df2e9a768c9be21b2f5c62f5a4
Konrad Michalik <kmichalikk19@gmail.com>
2025-08-23 23:10:34 +0200

    Simplify icons

--- src/ThreeEditor/components/Sidebar/SidebarTreeList/svg/Box.jsx
+++ src/ThreeEditor/components/Sidebar/SidebarTreeList/svg/Box.jsx
@@ -8,50 +8,33 @@ const SvgComponent = props => (
 		viewBox='0 0 36 36'
 		{...props}>
 		<path
-			d='m76.32 47.612-8.093 6.838'
+			d='m33.634 26.158-8.094 6.838'
 			style={{
 				fill: 'none',
-				strokeWidth: 2,
+				strokeWidth: 3,
 				strokeLinecap: 'round',
 				strokeLinejoin: 'round',
 				strokeDasharray: 'none'
 			}}
-			transform='translate(-42.686 -21.454)'
 		/>
 		<path
-			d='M45.046 31.303h23.159v23.159H45.046z'
+			d='M2.36 9.849h23.159v23.159H2.36z'
 			style={{
 				fill: 'none',
-				strokeWidth: 2,
+				strokeWidth: 3,
 				strokeLinejoin: 'round',
 				strokeDasharray: 'none'
 			}}
-			transform='translate(-42.686 -21.454)'
 		/>
 		<path
-			d='M53.166 23.445a1 1 0 0 0-1 1v4.858h2v-3.858h21.16v21.16h-5.121v2h6.121a1 1 0 0 0 1-1v-23.16a1 1 0 0 0-1-1Zm-1 9.858v14.302a1 1 0 0 0 1 1h13.04v-2h-12.04V33.303Z'
-			style={{
-				baselineShift: 'baseline',
-				display: 'inline',
-				overflow: 'visible',
-				opacity: 1,
-				vectorEffect: 'none',
-				strokeLinejoin: 'round',
-				enableBackground: 'accumulate',
-				stopOpacity: 1
-			}}
-			transform='translate(-42.686 -21.454)'
-		/>
-		<path
-			d='m53.146 24.46-8.093 6.838M76.284 24.47 68.19 31.31M53.184 47.557l-8.093 6.838'
+			d='m10.55 3.034-8.184 6.81M33.598 3.017l-8.094 6.838M10.595 3.014h23.052v22.882'
 			style={{
 				fill: 'none',
-				strokeWidth: 2,
+				strokeWidth: 3,
 				strokeLinecap: 'round',
 				strokeLinejoin: 'round',
 				strokeDasharray: 'none'
 			}}
-			transform='translate(-42.686 -21.454)'
 		/>
 	</svg>
 );
--- src/ThreeEditor/components/Sidebar/SidebarTreeList/svg/Cylinder.jsx
+++ src/ThreeEditor/components/Sidebar/SidebarTreeList/svg/Cylinder.jsx
@@ -7,38 +7,46 @@ const SvgComponent = props => (
 		height='36mm'
 		viewBox='0 0 36 36'
 		{...props}>
-		<g transform='translate(-73.495 -66.71)'>
-			<ellipse
-				cx={91.496}
-				cy={75.047}
-				rx={13.2}
-				ry={5.436}
+		<g
+			style={{
+				display: 'inline'
+			}}>
+			<path
+				d='M31.131 27.789s-.171.454-.7.905c-.527.451-1.434.957-2.631 1.39-2.394.866-5.908 1.454-9.798 1.454-3.89 0-7.406-.588-9.8-1.454-1.197-.433-2.104-.939-2.632-1.39-.528-.451-.686-.873-.686-.873'
 				style={{
+					baselineShift: 'baseline',
+					display: 'inline',
+					overflow: 'visible',
+					opacity: 1,
 					fill: 'none',
-					strokeWidth: 2,
+					strokeWidth: 3.1434,
 					strokeLinecap: 'round',
 					strokeLinejoin: 'round',
-					strokeDasharray: 'none'
-				}}
-			/>
-			<ellipse
-				cx={91.496}
-				cy={94.373}
-				rx={13.2}
-				ry={5.436}
-				style={{
-					fill: 'none',
-					strokeWidth: 2,
-					strokeLinecap: 'round',
-					strokeLinejoin: 'round',
-					strokeDasharray: 'none'
+					strokeDasharray: 'none',
+					enableBackground: 'accumulate',
+					stopOpacity: 1
 				}}
 			/>
 			<path
-				d='m78.29 75.237.016 19.167M104.683 75.237l.016 19.167'
+				d='M18.002 4.747c3.89 0 7.404.588 9.798 1.453 1.197.433 2.104.937 2.632 1.389.528.45.653.768.653 1.022 0 .253-.125.57-.653 1.022-.528.451-1.435.957-2.632 1.39-2.394.865-5.908 1.453-9.798 1.453-3.89 0-7.406-.588-9.8-1.453-1.197-.433-2.104-.939-2.632-1.39-.528-.452-.653-.769-.653-1.022 0-.254.125-.571.653-1.022.528-.452 1.435-.956 2.632-1.389 2.394-.865 5.91-1.453 9.8-1.453z'
 				style={{
+					baselineShift: 'baseline',
+					display: 'inline',
+					overflow: 'visible',
 					fill: 'none',
-					strokeWidth: 2,
+					strokeWidth: 3.1434,
+					strokeLinecap: 'round',
+					strokeLinejoin: 'round',
+					strokeDasharray: 'none',
+					enableBackground: 'accumulate'
+				}}
+			/>
+			<path
+				d='m4.796 8.527.016 19.167M31.188 8.527l.016 19.167'
+				style={{
+					display: 'inline',
+					fill: 'none',
+					strokeWidth: 3,
 					strokeLinecap: 'round',
 					strokeLinejoin: 'round',
 					strokeDasharray: 'none'
