0474b5efdedb25c30e6740f24ee00e31dba6f296
Konrad Michalik <kmichalikk19@gmail.com>
2025-08-26 22:11:35 +0200

    disable run simulation dialog when not logged in

--- src/WrapperApp/components/Simulation/RunSimulationPanel.tsx
+++ src/WrapperApp/components/Simulation/RunSimulationPanel.tsx
@@ -1,7 +1,9 @@
+import Block from '@mui/icons-material/Block';
 import CloudOff from '@mui/icons-material/CloudOff';
 import { AccordionDetails, AccordionSummary, Link, Typography, useTheme } from '@mui/material';
 
 import { useConfig } from '../../../config/ConfigService';
+import { useAuth } from '../../../services/AuthService';
 import { useStore } from '../../../services/StoreService';
 import StyledAccordion from '../../../shared/components/StyledAccordion';
 import { SimulatorNames, SimulatorType } from '../../../types/RequestTypes';
@@ -12,9 +14,13 @@ export default function RunSimulationPanel(props: RunSimulationFormProps) {
 	const theme = useTheme();
 	const { demoMode } = useConfig();
 	const { yaptideEditor } = useStore();
+	const { isAuthorized } = useAuth();
 
 	const showRunForm =
-		!demoMode || yaptideEditor?.contextManager.currentSimulator === SimulatorType.GEANT4;
+		(!demoMode && isAuthorized) ||
+		yaptideEditor?.contextManager.currentSimulator === SimulatorType.GEANT4;
+
+	console.log(showRunForm, isAuthorized);
 
 	return showRunForm ? (
 		<>
@@ -42,38 +48,63 @@ export default function RunSimulationPanel(props: RunSimulationFormProps) {
 					minHeight: 600,
 					padding: theme.spacing(2)
 				}}>
-				<CloudOff
-					color='disabled'
-					sx={{ width: 48, height: 48 }}
-				/>
-				<Typography color='textDisabled'>Demo Mode</Typography>
-				<Typography
-					color='textDisabled'
-					textAlign='center'>
-					Running{' '}
-					{yaptideEditor &&
-						SimulatorNames.get(yaptideEditor.contextManager.currentSimulator)}{' '}
-					is not possible in Demo Mode. For full version of the app, head to{' '}
-					<Link
-						color='secondary'
-						underline='none'
-						href={'https://yaptide.c3.plgrid.pl'}>
-						yaptide.c3.plgrid.pl
-					</Link>{' '}
-					or follow{' '}
-					<Link
-						color='secondary'
-						underline='none'
-						href={'https://yaptide.github.io/for_developers/'}>
-						installation steps
-					</Link>{' '}
-					to run it locally.
-				</Typography>
-				<Typography
-					color='textDisabled'
-					textAlign='center'>
-					Note that you can still use Geant4.
-				</Typography>
+				{demoMode && (
+					<>
+						<CloudOff
+							color='disabled'
+							sx={{ width: 48, height: 48 }}
+						/>
+						<Typography color='textDisabled'>Demo Mode</Typography>
+						<Typography
+							color='textDisabled'
+							textAlign='center'>
+							Running{' '}
+							{yaptideEditor &&
+								SimulatorNames.get(
+									yaptideEditor.contextManager.currentSimulator
+								)}{' '}
+							is not possible in Demo Mode. For full version of the app, head to{' '}
+							<Link
+								color='secondary'
+								underline='none'
+								href={'https://yaptide.c3.plgrid.pl'}>
+								yaptide.c3.plgrid.pl
+							</Link>{' '}
+							or follow{' '}
+							<Link
+								color='secondary'
+								underline='none'
+								href={'https://yaptide.github.io/for_developers/'}>
+								installation steps
+							</Link>{' '}
+							to run it locally.
+						</Typography>
+						<Typography
+							color='textDisabled'
+							textAlign='center'>
+							Note that you can still use Geant4.
+						</Typography>
+					</>
+				)}
+				{!demoMode && !isAuthorized && (
+					<>
+						<Block
+							color='disabled'
+							sx={{ width: 48, height: 48 }}
+						/>
+						<Typography color='textDisabled'>Login required</Typography>
+						<Typography
+							color='textDisabled'
+							textAlign='center'>
+							Running{' '}
+							{yaptideEditor &&
+								SimulatorNames.get(
+									yaptideEditor.contextManager.currentSimulator
+								)}{' '}
+							requires login.
+						</Typography>
+					</>
+				)}
 			</AccordionDetails>
 		</StyledAccordion>
 	);
