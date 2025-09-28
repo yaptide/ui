61d5321effa4a68c9cf9fb9ab6c4895433d8438b
Konrad Michalik <kmichalikk19@gmail.com>
2025-09-28 15:15:42 +0200

    Fix dependencies in useBackendAliveEffect

--- src/WrapperApp/components/Simulation/SimulationsGrid/hooks/useBackendAliveEffect.ts
+++ src/WrapperApp/components/Simulation/SimulationsGrid/hooks/useBackendAliveEffect.ts
@@ -3,19 +3,14 @@ import { useEffect, useState } from 'react';
 import { useConfig } from '../../../../../config/ConfigService';
 import { useRemoteWorkerSimulation } from '../../../../../services/RemoteWorkerSimulationContextProvider';
 import { useStore } from '../../../../../services/StoreService';
-import {
-	JobStatusData,
-	SimulationInfo,
-	ValidStatusStates
-} from '../../../../../types/ResponseTypes';
-import SimulationGridHelpers from '../SimulationsGridHelpers';
-import { SimulationConfig, SimulationHandlers, SimulationState } from '../SimulationsGridTypes';
+import { ValidStatusStates } from '../../../../../types/ResponseTypes';
+import { SimulationConfig, SimulationHandlers } from '../SimulationsGridTypes';
 
 export const useBackendAliveEffect = (
 	config: SimulationConfig,
 	handlers: SimulationHandlers,
-	updateSimulationInfo: () => void,
-	setPageCount: (count: number) => void
+	updateSimulationInfo: () => void = () => {},
+	setPageCount: (count: number) => void = () => {}
 ) => {
 	const { demoMode, controller, trackedId, isBackendAlive, setBackendAlive } = config;
 	const { getHelloWorld } = handlers;
@@ -34,9 +29,7 @@ export const useBackendAliveEffect = (
 	}, [
 		controller.signal,
 		getHelloWorld,
-		updateSimulationInfo,
 		isBackendAlive,
-		setBackendAlive,
 		demoMode,
 		trackedId,
 		controller,
@@ -46,11 +39,9 @@ export const useBackendAliveEffect = (
 
 export const useIsBackendAlive = () => {
 	const { demoMode } = useConfig();
-	const { trackedId, setResultsSimulationData, setLocalResultsSimulationData } = useStore();
+	const { trackedId } = useStore();
 
 	const [isBackendAlive, setBackendAlive] = useState(false);
-	const [simulationInfo, setSimulationInfo] = useState<SimulationInfo[]>([]);
-	const [simulationsStatusData, setSimulationsStatusData] = useState<JobStatusData[]>();
 	const [controller] = useState(new AbortController());
 
 	const config: SimulationConfig = {
@@ -64,20 +55,7 @@ export const useIsBackendAlive = () => {
 
 	const handlers = useRemoteWorkerSimulation();
 
-	const state: SimulationState = {
-		simulationInfo,
-		simulationsStatusData,
-		setSimulationInfo,
-		setSimulationsStatusData,
-		setResultsSimulationData,
-		setLocalResultsSimulationData,
-		setInputFiles: () => {},
-		setShowInputFilesEditor: () => {}
-	};
-
-	const { updateSimulationInfo, setPageCount } = SimulationGridHelpers(config, handlers, state);
-
-	useBackendAliveEffect(config, handlers, updateSimulationInfo, setPageCount);
+	useBackendAliveEffect(config, handlers);
 
 	return isBackendAlive;
 };
