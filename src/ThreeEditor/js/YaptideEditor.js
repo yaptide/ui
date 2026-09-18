b2a60a9959cd17588cf079e375a8742f130cc7a7
kanitsch <karolina.nitsch@gmail.com>
2026-09-18 18:15:21 +0200

    Update json version

--- src/ThreeEditor/js/YaptideEditor.js
+++ src/ThreeEditor/js/YaptideEditor.js
@@ -23,7 +23,7 @@ _DEFAULT_CAMERA.name = 'Camera';
 _DEFAULT_CAMERA.position.set(0, 5, 10);
 _DEFAULT_CAMERA.lookAt(new THREE.Vector3());
 
-export const JSON_VERSION = `0.12`;
+export const JSON_VERSION = `0.13`;
 
 export function YaptideEditor(container) {
 	this.signals = {
